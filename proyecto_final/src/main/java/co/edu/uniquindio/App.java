60fa0aab8cd8641bc238053fa97fb8eada246447
JPabloLG <juanp.lopezg@uqvirtual.edu.co>
2024-09-29 13:52:57 -0500

    Creacion de usuarioView
    Se creó la interfaz de la ventana principal del usuario

--- proyecto_final/src/main/java/co/edu/uniquindio/App.java
+++ proyecto_final/src/main/java/co/edu/uniquindio/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("inicioSesion"), 800, 500);
+        scene = new Scene(loadFXML("usuarioView"), 800, 500);
         stage.setScene(scene);
         stage.show();
     }
---
+++ proyecto_final/src/main/java/co/edu/uniquindio/Controller/inicioSesionController.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.Controller;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class inicioSesionController {
+
+}
